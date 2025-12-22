a0336c68c956dced318b76c616e8c5752830b460
sharma-sugurthi <sugurthimanisharma@gmail.com>
2025-12-22 23:07:43 +0530

    Add auto superuser creation

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
-
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser automatically if it doesn't exist
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin123') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell
